b188dc8bc09b9113fbd422dd39cb37a8e073b01a
Wils Iglesias <wiglesias83@gmail.com>
2022-11-30 23:40:22 +0100

    Create route with annotations

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -6,9 +6,12 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HealthCheckController
+
 {
+	#[Route('/', name: 'health_check', methods: ['GET'])]
 	public function __invoke(): Response
 	{
 		return new JsonResponse(['status' => 'ok']);
