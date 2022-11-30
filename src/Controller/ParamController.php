7e94c27dbfa18f53bc079cc40eeb69e3a9b84098
Wils Iglesias <wiglesias83@gmail.com>
2022-11-30 23:50:05 +0100

    Get URL parameters

---
+++ src/Controller/ParamController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParamController
+{
+	#[Route('/query', name: 'get-query-aram', methods: ['GET'])]
+	public function getQueryParams(Request $request): Response
+	{
+		$name = $request->query->get('name');
+		$email = $request->query->get('email');
+
+		return new JsonResponse([
+			'name' => $name,
+			'email' => $email
+		]);
+	}
+}
