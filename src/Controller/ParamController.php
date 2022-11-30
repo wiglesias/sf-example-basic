075623275f894fa78e0673e1ee42a13a66e77782
Wils Iglesias <wiglesias83@gmail.com>
2022-12-01 00:15:38 +0100

    Gets the request body

--- src/Controller/ParamController.php
+++ src/Controller/ParamController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,6 +37,17 @@ class ParamController
 		return new JsonResponse([
 			'name' => $name,
 			'email' => $email
+		]);
+	}
+
+	#[Route('/body', name: 'get-from-body', methods: ['POST'])]
+	public function getFromBody(Request $request): Response
+	{
+		$request->request = new ParameterBag(json_decode($request->getContent(), true));
+
+		return new JsonResponse([
+			'name' => $request->request->get('name'),
+			'email' => $request->request->get('email')
 		]);
 	}
 }
