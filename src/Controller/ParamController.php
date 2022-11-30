dc62957f3983fd0dd377aa73687e5347e49c6a6b
Wils Iglesias <wiglesias83@gmail.com>
2022-12-01 00:17:58 +0100

    Add prefix to the route

--- src/Controller/ParamController.php
+++ src/Controller/ParamController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/params')]
 class ParamController
 {
 	#[Route('/query', name: 'get-query-aram', methods: ['GET'])]
