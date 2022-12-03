c535340e99a1574d2ceab805eb93a7ddb628de9e
Wils Iglesias <wiglesias83@gmail.com>
2022-12-03 13:13:10 +0100

    Add category in the product form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,6 +18,10 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('sku')
             ->add('price')
+			->add('Category', EntityType::class, [
+				'class' => Category::class,
+				'choice_label' => 'name'
+			])
 			->add('save', SubmitType::class)
         ;
     }
